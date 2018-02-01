99eed94d7b4d39de361e9bf14982402be28891ab
Neve Hall <nhall27@uwo.ca>
2018-02-01 10:41:23 -0500

    Initial commit

---
+++ Assets/_Scripts/ResetPosition.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition: MonoBehaviour {
+
+	Vector3 startPos;
+
+	void Start () {
+		startPos = transform.position;
+
+	}
+
+	void OnMouseDown(){
+		gameObject.transform.position = startPos;
+	}
+
+}
---
+++ Assets/_Scripts/ResetRotation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetRotation : MonoBehaviour {
+
+	float zRotation = -12f;
+
+	void OnMouseDown(){
+		zRotation +=5f;
+		transform.parent.eulerAngles = new Vector3(5, zRotation, 0);
+	}
+}
+
